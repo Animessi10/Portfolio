3fc49a39023b6ce4c28d344da2a85b6f1b220ab9
Aniket <arijitmitra10kgp@gmail.com>
2026-08-25 02:45:52 +0530

    Initial portfolio setup

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Portfolio JavaScript is working!");
+
+const button = document.getElementById("helloButton");
+
+button.addEventListener("click", function () {
+    alert("JavaScript is working!");
+});
